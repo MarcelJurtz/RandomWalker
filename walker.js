26def77f14d3be4c1f5476fe82d145504e0a1aa0
MarcelJurtz <jurtzmarcel@gmail.com>
2017-02-19 18:49:01 +0100

    Implement OO-Approach

--- walker.js
+++ walker.js
@@ -1,44 +1,68 @@
 var x;
 var y;
-
+var walkers = [];
 
 
 function setup() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  console.log("W: " + width + " - H: " + height);
-  createCanvas(width, height);
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  createCanvas(w, h);
   background(50);
-  x = width / 2;
-  y = height / 2;
+  x = w / 2;
+  y = h / 2;
+
+  for(var i = 0; i < 5; i++) {
+    walkers.push(new Walker());
+  }
+
+  for(var i = 0; i < walkers.length; i++) {
+    walkers[i].drawPos();
+  }
 }
 
 function draw() {
-  stroke(255);
-  strokeWeight(6);
-  point(x,y);
-
-  var r = floor(random(4)); // Min: Default 0
-  var inc = 3;
-
-  switch(r) {
-    case 0:
-      y = y -inc;
-      break;
-    case 1:
-      x = x +inc;
-      break;
-    case 2:
-      y = y +inc;
-      break;
-    case 3:
-      x = x -inc;
-      break;
+  // Hide Scrollbar
+  document.body.style.overflow = 'hidden';
+  for(var i = 0; i < walkers.length; i++) {
+    console.log("for-loop");
+    walkers[i].move();
   }
+}
 
-  if(x > width) x = width;
-  if(x < 0) x = 0;
-  if(y > height) y = height;
-  if(y < 0) y = 0;
+function Walker() {
+  this.x = random(width);
+  this.y = random(height);
+  this.weight = 6;
 
+  this.move = function(){
+    console.log("Move!");
+    var r = floor(random(4)); // Min: Default 0
+    var inc = 3;
+
+    switch(r) {
+      case 0:
+        this.y = this.y -inc;
+        break;
+      case 1:
+        this.x = this.x +inc;
+        break;
+      case 2:
+        this.y = this.y +inc;
+        break;
+      case 3:
+        this.x = this.x -inc;
+        break;
+    }
+    if(this.x > window.innerWidth) this.x = window.innerWidth;
+    if(this.x < 0) this.x = 0;
+    if(this.y > window.innerHeight) this.y = window.innerHeight;
+    if(this.y < 0) this.y = 0;
+    this.drawPos();
+  };
+
+  this.drawPos = function() {
+    stroke(255);
+    strokeWeight(this.weight);
+    point(this.x, this.y);
+  };
 }
